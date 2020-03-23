46fe9934d2e27ae7643e340b4866de118c8ff507
Neutrovertido <russellcode.james@gmail.com>
2020-03-23 00:25:56 -0600

    Fully documented functions

--- file.cpp
+++ file.cpp
@@ -1,6 +1,32 @@
 // Include libraries
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include "functions.hpp"
+#include "functions.cpp"
+
+// To declare a function:
+// return_type function_name(parameters) {instructions}
+void say(std::string who, std::string msg) {
+    std::cout << who << ": " << msg << "\n";
+}
+
+std::string degozaru(std::string phrase) {
+    return phrase + "-degozaru";
+}
+
+// You can actually add a default value by assigning a value to num
+// This works when you want to make an argument optional so that is not entered every single time
+std::vector<int> quantum3(int num = 2) {
+    std::vector<int> exit;
+    for (int i=0; i<3; i++) {
+        exit.push_back(pow(num, i));
+    }
+    return exit;
+}
+
+// You can also define the variable and later the instructions (See under the main() method)
+double listAverage(std::vector<double> vec);
 
 int main() { // The main function
     // What the program actually does
@@ -63,5 +89,41 @@ int main() { // The main function
     even.push_back(12);
 
     std::cout << even[1] << "\n" << even.size() << "\n";
+
+    // Functions
+    std::cout << "The square root of 64 is: " << sqrt(64) << "\n";
+    // To see how to define a function look above the main() method
+    say("Gopnik","No u");
+
+    std::string daPhrase = "Hello my fellow pirate";
+    std::cout << degozaru(daPhrase) << "\n";
+    std::cout << listAverage({5, 4, 7, 12}) << "\n";
+
+    // This is actually the function that we declared in the external files
+    int thisIsSumm = theSum(5, 7);
+    std::cout << "The sum of 5 and 7 is: " << thisIsSumm << "\n";
+
+    // This is the second case of the function that it's being used when we pass doubles instead of ints
+    double decimalSum = theSum(3.65, 8.94);
+    std::cout << "The decimal sum results in: " << decimalSum << "\n";
+
+    // This one is declared in headers++ and is using inline
+    bigMultiplication(12);
+
+    // Template function
+    std::cout << greater("Leo ", "Messi") << "\n";
+    std::cout << greater(7, 14) << "\n";
+    std::cout << greater(44.5, 86.7) << "\n";
+}
+
+// See?
+// And also you can define and/or declare functions on other files! 
+double listAverage(std::vector<double> vec) {
+    double average = 0.0;
+    for (int i=0; i<vec.size(); i++){
+        average = average + i;
+    }
+    average = average / vec.size();
+    return average;
 
 }
---
+++ functions.cpp
@@ -0,0 +1,8 @@
+// See the functions declared
+int theSum(int num1, int num2) {
+    return num1 + num2;
+}
+
+double theSum(double num1, double num2) {
+    return num1 + num2;
+}
